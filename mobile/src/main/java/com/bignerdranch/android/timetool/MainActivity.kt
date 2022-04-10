08bfd2709cfae946dc5cce69c7d2984ff9c696d0
Oleg Dontsov <o.dontsov@samsung.com>
2022-04-10 18:22:21 +0300

    Add buttons "work" & "rest"

--- mobile/src/main/java/com/bignerdranch/android/timetool/MainActivity.kt
+++ mobile/src/main/java/com/bignerdranch/android/timetool/MainActivity.kt
@@ -3,10 +3,17 @@ package com.bignerdranch.android.timetool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.bignerdranch.android.timetool.ui.theme.TimeToolTheme
 
@@ -26,7 +33,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Row(modifier = Modifier.fillMaxHeight(), verticalAlignment = Alignment.CenterVertically) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Hello $name!")
+            Button(onClick = { }) {
+                Text(text = "Work")
+            }
+            Button(onClick = { }) {
+                Text(text = "Rest")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
