1b6c22ee0466e71b554bb921f55557460d367021
Oleg Dontsov <o.dontsov@samsung.com>
2022-04-10 17:20:55 +0300

    Add start compose UI

--- mobile/src/main/java/com/bignerdranch/android/timetool/MainActivity.kt
+++ mobile/src/main/java/com/bignerdranch/android/timetool/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.bignerdranch.android.timetool
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.bignerdranch.android.timetool.ui.theme.TimeToolTheme
 
-class MainActivity : AppCompatActivity()  {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            TimeToolTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DefaultPreview() {
+    TimeToolTheme {
+        Greeting("Android")
     }
 }
--- wear/src/main/java/com/bignerdranch/android/timetool/MainActivity.kt
+++ wear/src/main/java/com/bignerdranch/android/timetool/MainActivity.kt
@@ -1,18 +1,39 @@
 package com.bignerdranch.android.timetool
 
-import android.app.Activity
+
 import android.os.Bundle
-import com.bignerdranch.android.timetool.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.bignerdranch.android.timetool.ui.theme.TimeToolTheme
 
-class MainActivity : Activity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            TimeToolTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+@Composable
+private fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
 
+@Preview(showBackground = true)
+@Composable
+private fun DefaultPreview() {
+    TimeToolTheme {
+        Greeting("Android")
     }
 }
